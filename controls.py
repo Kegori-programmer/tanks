49b15514768425df934d9dad97f7f71067388d63
Ivan Kosenko <foxartorg@gmail.com>
2022-01-27 17:42:35 +0200

    papa ne pomnit

--- controls.py
+++ controls.py
@@ -1,17 +1,24 @@
 import pygame
 
 
-def keys(event, result):
-
+def keys(event, position):
     if event.type == pygame.QUIT:
         exit()
     if event.type == pygame.KEYDOWN:
-        if event.key == pygame.K_LEFT:
-            result = 'left'
-        elif event.key == pygame.K_RIGHT:
-            result = 'right'
+        if event.key == pygame.K_UP:
+            position['title'] = 'up'
+            position['x'] = 0
+            position['y'] = 1
         elif event.key == pygame.K_DOWN:
-            result = 'down'
-        elif event.key == pygame.K_UP:
-            result = 'up'
-    return result
+            position['title'] = 'down'
+            position['x'] = 0
+            position['y'] = -1
+        elif event.key == pygame.K_LEFT:
+            position['title'] = 'left'
+            position['x'] = -1
+            position['y'] = 0
+        elif event.key == pygame.K_RIGHT:
+            position['title'] = 'right'
+            position['x'] = 1
+            position['y'] = 0
+    return position
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+
 import controls
 
 pygame.init()
@@ -6,16 +7,22 @@ pygame.init()
 # Set up the drawing window
 screen = pygame.display.set_mode([500, 500])
 font = pygame.font.Font('res/font.ttf', 25)
-result = ''
+
+position = {'x': 0, 'y': 0, 'title': ''}
+
 while True:
     # Fill the background with white
     screen.fill((255, 255, 255))
     # Did the user click the window close button?
     for event in pygame.event.get():
-        result = controls.keys(event, result)
+        position = controls.keys(event, position)
     # Draw a solid blue circle in the center
     # pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
-    title = font.render(str(result), True, pygame.Color('black'))
+    title = font.render(str(position['title']), True, pygame.Color('black'))
+    x = font.render('x: ' + str(position['x']), True, pygame.Color('black'))
+    y = font.render('y: ' + str(position['y']), True, pygame.Color('black'))
     screen.blit(title, (100, 100))
+    screen.blit(x, (100, 125))
+    screen.blit(y, (100, 150))
     # Flip the display
     pygame.display.flip()
