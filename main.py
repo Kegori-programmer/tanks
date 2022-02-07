fe26255325b85f8cc6beb2bf9dbdffbb4d38f86f
Ivan Kosenko <foxartorg@gmail.com>
2022-02-07 13:45:00 +0200

    fix

--- main.py
+++ main.py
@@ -2,11 +2,10 @@ from classes.GraphicClass import Graphic
 from classes.InfoClass import Info
 from classes.TankBotClass import TankBot
 from classes.TankPlayerClass import TankPlayer
-from helpers.MathHelper import Math
 from helpers.PygameHelper import Pygame
 
 
-speed = 10
+speed = 5
 width = 1024
 height = 768
 tank_size = 15
@@ -21,10 +20,8 @@ bot_2 = TankBot(graphic, 'bot_2', round(width / 2), round(height / 10), tank_siz
 bot_3 = TankBot(graphic, 'bot_3', round(width / 10), round(height / 10), tank_size, bot_velocity / 1.3, (187, 62, 3))
 bot_4 = TankBot(graphic, 'bot_4', round(width / 2), round(height / 5), tank_size, bot_velocity / 1.4, (202, 103, 2))
 bot_5 = TankBot(graphic, 'bot_5', round(width / 5), round(height / 2), tank_size, bot_velocity / 1.5, (238, 155, 0))
-bots = [bot_1, bot_2, bot_3, bot_4, bot_5]
-
-
-# bots = [bot_1, bot_2, bot_3]
+# bots = [bot_1, bot_2, bot_3, bot_4, bot_5]
+bots = [bot_1, bot_2, bot_3]
 # bots = [bot_1]
 
 
@@ -46,10 +43,11 @@ def logic():
     # Bot
     for bot in bots:
         pos_bot_x, pos_bot_y = bot.get_position(plr_1)
-        if Math.distance_scalar(bot.pos, plr_1.pos) > bot.size + plr_1.size:
-            bot.set_position(pos_bot_x, pos_bot_y)
-        else:
-            exit()
+        bot.set_position(pos_bot_x, pos_bot_y)
+        # if Math.distance_scalar(bot.pos, plr_1.pos) > bot.size + plr_1.size:
+        # bot.set_position(pos_bot_x, pos_bot_y)
+        # else:
+        #     exit()
 
 
 def run():
